1005dd07d9207e23c667a6aca44c4b96411bd35b
zhujjotw <18557537174@163.com>
2021-03-06 11:13:08 +0800

    xiugai

--- renxi/functions.js
+++ renxi/functions.js
@@ -38,27 +38,25 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	// var current = Date();
-	// var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var current = Date();
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	// var days = Math.floor(seconds / (3600 * 24));
-	// seconds = seconds % (3600 * 24);
-	// var hours = Math.floor(seconds / 3600);
-	// if (hours < 10) {
-	// 	hours = "0" + hours;
-	// }
-	// seconds = seconds % 3600;
-	// var minutes = Math.floor(seconds / 60);
-	// if (minutes < 10) {
-	// 	minutes = "0" + minutes;
-	// }
-	// seconds = seconds % 60;
-	// if (seconds < 10) {
-	// 	seconds = "0" + seconds;
-	// }
-
 	var days = 169;
+	seconds = seconds % (3600 * 24);
+	var hours = Math.floor(seconds / 3600) + 10;
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	seconds = seconds % 3600;
+	var minutes = Math.floor(seconds / 60);
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	seconds = seconds % 60;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
 
-
-	var result = "我们在一起第 <span class=\"digit\">" + days + "</span> 天"; 
+	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
